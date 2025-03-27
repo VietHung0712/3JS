1b7571a9e8b1d2044e9d4e34dcf56b7e98dbd651
VietHung0712 <quyvuongbn@gmail.com>
2025-03-27 16:06:46 +0700

    Update

--- src/function.js
+++ src/function.js
@@ -1,6 +1,22 @@
+const $ = document.querySelector.bind(document);
+
 function toDegrees(radians) {
     return radians * (180 / Math.PI);
 }
 function toRadians(degrees) {
     return degrees * Math.PI / 180;
 }
+
+function toggleFullScreen() {
+    if (!document.fullscreenElement) {
+        document.documentElement.requestFullscreen().catch(err => {
+            console.error(`Lỗi khi vào toàn màn hình: ${err.message}`);
+        });
+        $("#menu").style.display = "none";
+    } else {
+        document.exitFullscreen();
+        $("#menu").style.display = "flex";
+    }
+}
+
+$("#fullscreenButton").addEventListener("click", toggleFullScreen);
